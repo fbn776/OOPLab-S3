357dc1b0aa647ca3015d2b3dfa179da021e827b6
fbn776 <fbn776@gmail.com>
2023-10-15 18:57:07 +0530

    Changes to Inheritance.java

--- B/Inheritance.java
+++ B/Inheritance.java
@@ -1,9 +1,8 @@
-
 class Employee {
-    String Name, Address, PhoneNumber;
-    int Age, Salary;
+    char[] Name, Address;
+    int Age, Salary, PhoneNumber;
 
-    public Employee(String Name, int Age, String PhoneNumber, String Address, int Salary) {
+    public Employee(char[] Name, int Age, int PhoneNumber, char[] Address, int Salary) {
         this.Name = Name;
         this.Age = Age;
         this.PhoneNumber = PhoneNumber;
@@ -11,36 +10,78 @@ class Employee {
         this.Salary = Salary;
     }
 
-    public void printSalary() {
-        System.out.println("The salary is " + this.Salary);
+    final public void printSalary() {
+        System.out.println("Salary: " + this.Salary);
+    }
+    public void printDetails() {
+        System.out.print("Name: ");
+        Employee.printItem(Name);
+        System.out.println("Age: " + Age);
+        System.out.println("Phone: " + PhoneNumber);
+        System.out.print("Address: ");
+        Employee.printItem(Address);
+        this.printSalary();
+    }
+
+    static void printItem(char[] item) {
+        for(int i = 0; item[i] != '\0'; i++) {
+            System.out.print(item[i]);
+        }
+        System.out.println();
     }
 }
 
 class Officer extends Employee {
-    String specialization, department;
+    char[] specialization, department;
 
-    public Officer(String Name, int Age, String PhoneNumber, String Address, int Salary, String specialization, String department) {
+    public Officer(char[] Name, int Age, int PhoneNumber, char[] Address, int Salary, char[] specialization, char[] department) {
         super(Name, Age, PhoneNumber, Address, Salary);
         this.specialization = specialization;
         this.department = department;
     }
+    public void printDetails() {
+        super.printDetails();
+        System.out.print("Specialization: ");
+        Employee.printItem(specialization);
+        System.out.print("Department: ");
+        Employee.printItem(department);
+    }
 }
 
 class Manager extends Employee {
-    String specialization, department;
-    public Manager(String Name, int Age, String PhoneNumber, String Address, int Salary, String specialization, String department) {
+    char[] specialization, department;
+
+    public Manager(char[] Name, int Age, int PhoneNumber, char[] Address, int Salary, char[] specialization, char[] department) {
         super(Name, Age, PhoneNumber, Address, Salary);
         this.specialization = specialization;
         this.department = department;
+    }
+
+    public void printDetails() {
+        super.printDetails();
+        System.out.print("Specialization: ");
+        Employee.printItem(specialization);
+        System.out.print("Department: ");
+        Employee.printItem(department);
     }
 }
 
 public class Inheritance {
     public static void main(String[] args) {
-        Officer officer1 = new Officer("Ham", 30, "388399283", "Home", 83729, "Lead", "HR");
-        Manager manager1 = new Manager("Jam", 32, "324324324", "Work", 49399, "Team lead", "Market");
+        char[] officeName = {'H', 'a', 'm', '\0'},
+                officeAddress = {'H', 'o', 'm', 'e', '\0'},
+                officeSpec = {'L', 'e', 'a', 'd', '\0'},
+                officeDept = {'H', 'R', '\0'},
+                managerName = {'S', 'a', 'm', '\0'},
+                mangerAddress = {'W', 'o', 'r', 'k', '\0'},
+                managerSpec = {'T', 'e', 's', 't', '\0'},
+                managerDept = {'D', 'e', 'v', '\0'};
 
-        officer1.printSalary();
-        manager1.printSalary();
+
+        Officer officer1 = new Officer(officeName, 32, 874827, officeAddress, 456728, officeSpec, officeDept);
+        Manager manager1 = new Manager(managerName, 40, 8489209, mangerAddress, 838883, managerSpec, managerDept);
+
+        officer1.printDetails();
+        manager1.printDetails();
     }
 }
